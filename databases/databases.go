742eeb43dbb99812b6bcfc89710d65e6b262f4d0
jason <1475351244@qq.com>
2020-12-04 21:40:02 +0800

    API更新

--- databases/databases.go
+++ databases/databases.go
@@ -10,7 +10,7 @@ import (
 func Connect() *gorm.DB {
 	db, err := gorm.Open("mysql", "root:1e80a9ad15576fb9@tcp(127.0.0.1)/gin?charset=utf8mb4&parseTime=True&loc=Local")
 
-	if err != nil {
+	if err != nil { 
 		fmt.Println("MYSQL连接失败了！")
 		log.Fatal(err)
 	} else {
